e6497e23e393608766b588598e4bd67aed6a0511
Jake Waksbaum <jake.waksbaum@gmail.com>
2014-05-28 15:35:20 -0400

    remove devlog from git

--- setup-log.sh
+++ setup-log.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 name='devlog'
-echo -e "#!/bin/bash\ngit log > ./"$name".txt\ngit add "$name".txt" > .git/hooks/pre-commit
-chmod +x .git/hooks/pre-commit
+echo -e "#!/bin/bash\ngit log > ./"$name".txt" > .git/hooks/post-commit
+chmod +x .git/hooks/post-commit
