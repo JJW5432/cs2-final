783d8ef53776bfd96819e3958efd1df95dabb0b2
Jake Waksbaum <jake.waksbaum@gmail.com>
2014-05-28 15:23:30 -0400

    renamed log to devlog

--- setup-log.sh
+++ setup-log.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-echo -e "#!/bin/bash\ngit log > ~/cs2-final/log.txt\ngit add log.txt" > .git/hooks/pre-commit
+name='devlog'
+echo -e "#!/bin/bash\ngit log > ~/cs2-final/"$name".txt\ngit add "$name".txt" > .git/hooks/pre-commit
 chmod +x .git/hooks/pre-commit
