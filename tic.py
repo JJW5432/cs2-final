96874a0054e273243763aa37386e64fb305758de
Jake Waksbaum <jake.waksbaum@gmail.com>
2014-05-30 11:00:52 -0400

    complete rewrite to use dictionary representation with tuples coords as keys instead of list of lists

--- tic.py
+++ tic.py
@@ -1,4 +1,4 @@
-a_board = [['1','2', '3'],['4','5','6'],['7', '8', '9']]
+a_board = {(-1,1): '1', (0,1): '2', (1,1): '3', (-1,0):'4', (0,0): '5', (1,0): '6', (-1,-1): '7', (0,-1): '8', (1,-1): '9'}
 def display(board,sep=''):
 	"""displays a textual representation of a board (for dev purposes)
 	>>> display(a_board)
@@ -6,58 +6,70 @@ def display(board,sep=''):
 	4|5|6
  	7|8|9
 	"""
-	for row in board:
-		print "|".join(row)
+        for n in [1,0,-1]:
+                print "|".join([board[point] for point in sorted(board.keys()) if point[1] == n])
 	if len(sep) > 0: print sep
 
-def rotate(board,n=1):
+def rotate(cell, n=1):
+        """given the coordiantes of a cell, it returns the coordinates of the cell after a n rotations of 90 degrees clockwise"""
+        for x in range(n):
+                cell = (cell[1], -1*cell[0])
+        return cell
+
+def rotate_board(board,n=1):
 	"""rotates a given board clockwise 90 degrees n times
-	>>> display(rotate(a_board))
+	>>> display(rotate_board(a_board))
 	7|4|1
 	8|5|2
 	9|6|3
-	>>> display(rotate(a_board,2))
+	>>> display(rotate_board(a_board,2))
 	9|8|7
 	6|5|4
 	3|2|1
-	>>> rotate(a_board,4)==a_board
+	>>> rotate_board(a_board,4)==a_board
 	True
 	"""
-	for x in range(n):
-		board = [[row[n] for row in board][::-1] for n in range(len(board))]
+        backup = board.copy()
+        board = {rotate(point,n): backup[point] for point in backup.keys()}
 	return board
 
-def reflect(board,d):
+def reflect(cell,d):
+	if d == 'v': #vertical
+		return (-1*cell[0],cell[1])
+	elif d == 'h': #horizontal
+		return (cell[0], -1*cell[1])
+	elif d =='l': #top-left to bottom-right
+		return (-1*cell[1], -1*cell[0])
+	elif d == 'r': #top-right to bottom-left
+		return (cell[1], cell[0])
+
+
+def reflect_board(board,d):
 	"""reflects board accross vertical ('v'), horizontal ('h'), top-left to bottom-right diagonal ('l'), or top-right to bottom-left diagonal ('r')
-	>>> display(reflect(a_board,'v'))
+	>>> display(reflect_board(a_board,'v'))
 	3|2|1
 	6|5|4
 	9|8|7
-	>>> display(reflect(a_board,'h'))
+	>>> display(reflect_board(a_board,'h'))
 	7|8|9
 	4|5|6
 	1|2|3
-	>>> display(reflect(a_board,'l'))
+	>>> display(reflect_board(a_board,'l'))
 	1|4|7
 	2|5|8
 	3|6|9
-	>>> display(reflect(a_board,'r'))
+	>>> display(reflect_board(a_board,'r'))
 	9|6|3
 	8|5|2
 	7|4|1
 	"""
-	if d == 'v': #vertical
-		return [row[::-1] for row in board]
-	elif d == 'h': #horizontal
-		return board[::-1]
-	elif d =='l': #top-left to bottom-right
-		return [[row[n] for row in board] for n in range(len(board))]
-	elif d == 'r': #top-right to bottom-left
-		return [[row[-(n+1)] for row in board][::-1] for n in range(len(board))]
+        backup = board.copy()
+        board = {reflect(point,d): backup[point] for point in backup.keys()}
+        return board
 
 def isoboards(board):
 	"""returns a list of all equivelant boards"""
-	return [board, rotate(board), rotate(board,2), rotate(board,3), reflect(board,'v'), reflect(board,'h'), reflect(board,'l'), reflect(board,'r')]
+	return [board, rotate_board(board), rotate_board(board,2), rotate_board(board,3), reflect_board(board,'v'), reflect_board(board,'h'), reflect_board(board,'l'), reflect_board(board,'r')]
 
 def is_isomorphic(board1, board2):
 	return board1 in isoboards(board2)
