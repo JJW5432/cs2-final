9193e1b9caaa702adab583815cce2cdd08dbb515
Jake Waksbaum <jake.waksbaum@gmail.com>
2014-06-02 19:12:19 -0400

    added tests for everything and passing

--- tic.py
+++ tic.py
@@ -14,6 +14,7 @@ class Cell:
         return hash( (self.coords, self.filled()) )
     
     def __eq__(self, other): #doesn't matter if it's mine or yours
+        if type(self) != type(other): return False
         return (self.x, self.y, self.filled()) == (other.x, other.y, other.filled())
 
     def __ne__(self, other):
@@ -29,7 +30,10 @@ class Cell:
         return "<Cell (" + str(self.x) + "," + str(self.y) + ") " + str(self.state) + ">"
 
     def __invert__(self):
-        return Cell(self.coords[0], self.y, self.state*-1)
+        if self.string == 'x': string = 'o'
+        elif self.string == 'o': string = 'x'
+        else: string = self.string
+        return Cell(self.coords[0], self.y, self.state*-1, string)
 
     def empty(self):
         return self.state == 0
@@ -43,9 +47,9 @@ class Cell:
     def rotate(self, n=1):
         """given the coordiantes of a cell, it returns the coordinates of the cell after a n rotations of 90 degrees clockwise"""
         (x,y) = self.coords
-        for x in range(n):
-                cell = (y, -1*x)
-        return Cell(x,y, self.state)
+        for i in range(n):
+                (x,y) = (y, -1*x)
+        return Cell(x,y, self.state,self.string)
 
     def reflect(self,d):
         (x,y) = self.coords
@@ -57,11 +61,11 @@ class Cell:
             (x,y) = (-1*y, -1*x)
         elif d == 'r': #top-right to bottom-left
             (x,y) = (y, x)
-        return Cell(x, y, self.state)
+        return Cell(x, y, self.state, self.string)
 
 class Board:
     def __init__(self, cells):
-        self.cells = cells
+        self.cells = sorted(cells, key= lambda cell: cell.coords)
 
     @classmethod
     def from_dict(cls,d):
@@ -69,92 +73,45 @@ class Board:
         return Board( [Cell(coord[0], coord[1], (states[d[coord]] if d[coord] in states else 0), d[coord]) for coord in d.keys()] )
 
     def isoboards(self):
-        {self: lambda x: x, self.rotate(): lambda x: x.rotate(), self.rotate(2): lambda x: x.rotate(2), self.rotate(3): lambda x: x.rotate(3), self.reflect('v'): lambda x: x.reflect('v'), self.reflect('h'): lambda x: x.reflect('h'), self.reflect('l'): lambda x: x.reflect('l'), self.reflect('r'): lambda x: x.reflect('r')}
+        return {self: lambda x: x, self.rotate(): lambda x: x.rotate(), self.rotate(2): lambda x: x.rotate(2), self.rotate(3): lambda x: x.rotate(3), self.reflect('v'): lambda x: x.reflect('v'), self.reflect('h'): lambda x: x.reflect('h'), self.reflect('l'): lambda x: x.reflect('l'), self.reflect('r'): lambda x: x.reflect('r')}
     def __hash__(self):
         return hash( tuple([hash(cell) for cell in self.cells]) )
     
     def __eq__(self,other):
+        if type(self) != type(other): return False
         return type(other) == type(self) and self.cells == other.cells
     
     def __ne__(self,other):
         return not self == other
 
+    def __invert__(self):
+        return Board(map(lambda x: ~x, self.cells))
+
     def is_isomorphic(self, other):
         return type(other) == type(self) and other in self.isoboards()
 
     def __str__(self):
-        """returns the string that should be printed when you print a board 
-        """
+        """returns the string that should be printed when you print a board """
         out = ''
         for n in [1,0,-1]:
              out += "|".join([str(point) for point in sorted(self.cells,key = lambda x: x.coords) if point.coords[1] == n])
              out +="\n"
-        return out.strip()
+        return out.strip('\n')
         
     def rotate(self,n=1):
-        """rotates a given board clockwise 90 degrees n times
-        >>> display(rotate_board(a_board))
-        7|4|1
-        8|5|2
-        9|6|3
-        >>> display(rotate_board(a_board,2))
-        9|8|7
-        6|5|4
-        3|2|1
-        >>> rotate_board(a_board,4)==a_board
-        True
-        """
+        """rotates a given board clockwise 90 degrees n times"""
         return Board( [cell.rotate(n) for cell in self.cells] )
 
     def reflect(self,d):
-        """reflects board accross vertical ('v'), horizontal ('h'), top-left to bottom-right diagonal ('l'), or top-right to bottom-left diagonal ('r')
-        >>> display(reflect_board(a_board,'v'))
-        3|2|1
-        6|5|4
-        9|8|7
-        >>> display(reflect_board(a_board,'h'))
-        7|8|9
-        4|5|6
-        1|2|3
-        >>> display(reflect_board(a_board,'l'))
-        1|4|7
-        2|5|8
-        3|6|9
-        >>> display(reflect_board(a_board,'r'))
-        9|6|3
-        8|5|2
-        7|4|1
-        """
+        """reflects board accross vertical ('v'), horizontal ('h'), top-left to bottom-right diagonal ('l'), or top-right to bottom-left diagonal ('r')"""
         return Board( [cell.reflect(d) for cell in self.cells] )
 
-    def randomMove(board):
-        """outputs a random unoccupied position given a board
-        >>> randomMove(board1)
-        [0, -1]
-        """
-        return random.choice([cell for cell in cells if not cell.filled])
+    def randomMove(self):
+        """outputs a random unoccupied position given a board"""
+        return random.choice([cell for cell in self.cells if not cell.filled()])
         
     def matchMove(self, other, move):
-        """finds equivalent move in isoboard
-        isoboards() function returns list of isoboards as following:
-        >>> matchMoveIsoboard((1,1),board2,board3)
-        (-1, 1)
-        >>> matchMoveIsoboard((1,1),board2,board1)
-        'not isomorphic'
-        >>> matchMoveIsoboard((1,1),board2,board2)
-        (1, 1)
-        """
-        if self != other:
+        """finds equivalent move in isoboard"""
+        if not self.is_isomorphic(other):
             return False
-        else: return self.isoboards[other](cell)
-
-
-a_board = Board.from_dict({(-1,1): '1', (0,1): '2', (1,1): '3', (-1,0):'4', (0,0): '5', (1,0): '6', (-1,-1): '7', (0,-1): '8', (1,-1): '9'})
-
-board1 = Board.from_dict({(-1,1):"x",(0,1):"o",(1,1):"x",(-1,0):"x",(0,0):"x",(1,0):"o",(-1,-1):"o",(0,-1):" ",(1,-1):"o"})
-board2 = Board.from_dict({(-1,1):"x",(0,1):"o",(1,1):" ",(-1,0):" ",(0,0):" ",(1,0):" ",(-1,-1):" ",(0,-1):" ",(1,-1):" "})  
-board3 = Board.from_dict({(-1,1):" ",(0,1):"o",(1,1):"x",(-1,0):" ",(0,0):" ",(1,0):" ",(-1,-1):" ",(0,-1):" ",(1,-1):" "})
-
-if __name__ == "__main__":
-    import doctest
-    doctest.testmod()
+        else: return self.isoboards()[other](move)
