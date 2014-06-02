cd4e622a94b4ba52138328ea0c2d290421e45d97
Jake Waksbaum <jake.waksbaum@gmail.com>
2014-06-02 08:42:57 -0400

    continued work on classes, almost done. need tests

--- tic.py
+++ tic.py
@@ -3,9 +3,13 @@ import random
 class Board:
     def __init__(self, cells):
         self.cells = cells
-        #self.isoboards = [board, rotate_board(board), rotate_board(board,2), rotate_board(board,3), reflect_board(board,'v'), reflect_board(board,'h'), reflect_board(board,'l'), reflect_board(board,'r')]
+        self.isoboards = {self: lambda x: x, self.rotate(): lambda x: x.rotate(), self.rotate(2): lambda x: x.rotate(2), self.rotate(3): lambda x: x.rotate(3), self.reflect('v'): lambda x: x.reflect('v'), self.reflect('h'): lambda x: x.reflect('h'), self.reflect('l'): lambda x: x.reflect('l'), self.reflect('r'): lambda x: x.reflect('r')}
+    
+    def __hash__(self):
+        return hash( tuple([hash(cell) for cell in self.cells]) )
+    
     def __eq__(self,other):
-        return type(other) == type(self) and (self.cells == other.cells or self.cells == [~cell for cell in other.cells])
+        return type(other) == type(self) and self.cells == other.cells
     
     def __ne__(self,other):
         return not self == other
@@ -20,9 +24,6 @@ class Board:
         for n in [1,0,-1]:
              out += "|".join([board[point] for point in sorted(board.keys()) if point[1] == n])
         return out
-
-    def __hash__(self):
-        return tuple(
         
     def rotate(self,n=1):
         """rotates a given board clockwise 90 degrees n times
@@ -66,26 +67,70 @@ class Board:
             [0, -1]
             """
             return random.choice([cell for cell in cells if not cell.filled])
+        
+        def matchMove(self, other, move):
+            """finds equivalent move in isoboard
+            isoboards() function returns list of isoboards as following:
+            >>> matchMoveIsoboard((1,1),board2,board3)
+            (-1, 1)
+            >>> matchMoveIsoboard((1,1),board2,board1)
+            'not isomorphic'
+            >>> matchMoveIsoboard((1,1),board2,board2)
+            (1, 1)
+            """
+            if self != other:
+                return False
+            return self.isoboards[other](cell)
 
         
 class Cell:
-    def __init__(self, x, y, filled=False, mine=False):
+    def __init__(self, x, y, state=0, string=''):
         self.coords = (x,y)
-        self.filled = filled
-        self.mine = filled and mine
+        self.x, self.y = x, y
+        self.state = state
     
     def __hash__(self):
-        return (self.x, self.y, 
+        return hash( (self.coords, self.filled()) )
+    
+    def __eq__(self, other): #doesn't matter if it's mine or yours
+        return (self.x, self.y, self.filled()) == (other.x, other.y, other.filled())
+
+    def __ne__(self, other):
+        return not self == other
+     
+    def __str__(self):
+        if len(self.string) > 0:
+            return self.string
+        else:
+            return ['x', ' ', 'o'][self.state]
+    
+    def __repr__(self):
+        return "<Cell at (" + str(x) + "," + str(y) + "), state: " + str(self.state) + ">"
+
+    def __invert__(self):
+        return Cell(self.coords[0], self.y, self.state*-1)
+
+    def filled(self):
+        return self.state != 0
+
+    def empty(self):
+        return self.state == 0
+    
+    def mine(self):
+        return state == 1
+
+    def theirs(self):
+        return state == -1
 
     def rotate(self, n=1):
         """given the coordiantes of a cell, it returns the coordinates of the cell after a n rotations of 90 degrees clockwise"""
-        (x,y) = self.pos
+        (x,y) = self.coords
         for x in range(n):
                 cell = (y, -1*x)
-        return Cell(x,y, self.filled, self.mine)
+        return Cell(x,y, self.state)
 
     def reflect(self,d):
-        (x,y) = self.cell
+        (x,y) = self.coords
         if d == 'v': #vertical
             (x,y) = (-1*x,y)
         elif d == 'h': #horizontal
@@ -94,56 +139,16 @@ class Cell:
             (x,y) = (-1*y, -1*x)
         elif d == 'r': #top-right to bottom-left
             (x,y) = (y, x)
-        return Cell(x, y, self.filled, self.mine)
+        return Cell(x, y, self.state)
 
 
 
 a_board = {(-1,1): '1', (0,1): '2', (1,1): '3', (-1,0):'4', (0,0): '5', (1,0): '6', (-1,-1): '7', (0,-1): '8', (1,-1): '9'}
-
-import random
+a_board = Board([Cell(x[0], x[1], 0, a_board[x]) for x in a_board.keys()])
 
 board1 = {(-1,1):"x",(0,1):"o",(1,1):"x",(-1,0):"x",(0,0):"x",(1,0):"o",(-1,-1):"o",(0,-1):" ",(1,-1):"o"}
 board2 = {(-1,1):"x",(0,1):"o",(1,1):" ",(-1,0):" ",(0,0):" ",(1,0):" ",(-1,-1):" ",(0,-1):" ",(1,-1):" "}   
 board3 = {(-1,1):" ",(0,1):"o",(1,1):"x",(-1,0):" ",(0,0):" ",(1,0):" ",(-1,-1):" ",(0,-1):" ",(1,-1):" "}
-
-def matchMoveIsoboard(move,board,isoboard):
-    """finds equivalent move in isoboard
-    isoboards() function returns list of isoboards as following:
-    0 - original
-    1 - rotated 90
-    2 - rotated 180
-    3 - rotated 270
-    4 - flipped on vertical
-    5 - flipped on horizontal
-    6 - flipped on top-left to bottom-right
-    7 - flipped on top-right to bottom-left
-    >>> matchMoveIsoboard((1,1),board2,board3)
-    (-1, 1)
-    >>> matchMoveIsoboard((1,1),board2,board1)
-    'not isomorphic'
-    >>> matchMoveIsoboard((1,1),board2,board2)
-    (1, 1)
-    """
-    try:
-        transformation = isoboards(board).index(isoboard)
-    except:
-        return "not isomorphic"
-    if transformation == 1:
-        return rotate(move,1)
-    elif transformation == 2:
-        return rotate(move,2)
-    elif transformation == 3:
-        return rotate(move,3)
-    elif transformation == 4:
-        return reflect(move,'v')
-    elif transformation == 5:
-        return reflect(move,'h')
-    elif transformation == 6:
-        return reflect(move,'l')
-    elif transformation == 7:
-        return reflect(move,'r')
-    else:
-        return move
 
 if __name__ == "__main__":
     import doctest
