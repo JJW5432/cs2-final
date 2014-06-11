385da8bef8ee796c22308fdef4209a6a9d80732f
askyel <ariel.s.levy1@gmail.com>
2014-06-11 00:49:47 -0400

    reworked play.py

--- play.py
+++ play.py
@@ -1,5 +1,38 @@
 from tic_lib import *
 
+fileMemory = open("memory.csv")
+memory = fileMemory.read()
+fileMemory.close()
+
+memory = memory.split("\n")
+i = 0
+for move in memory:
+    board = Board.from_string(move.split("'")[1])
+    memory[i] = [board] + move.split("'")[2].split(",")[1:]
+    i += 1
+
+def findMemory(b):
+    '''gets list of moves and outcomes in isomorphic situations'''
+    L = [] 
+    for move in memory: # list in memory
+        if Board.is_isomorphic(move[0],b): # tests if given board is same as board in memory
+            play = Board.matchMove(move[0],b,move[1]) # equivalent move in isomorphic board
+            newPlay = True # tests if new move needs to be added
+            for m in L:
+                if m[0] == play:
+                    if move[3] == 1:
+                        m[1] += 1.0
+                    elif move[3] == -1:
+                        m[2] += 1.0
+                    newPlay = False
+            if newPlay == True:
+                L += [[move[1],0.0,0.0,0.0,move[2]]]
+                if move[3] == 1:
+                    m[1] += 1.0
+                elif move[3] == -1:
+                    m[2] += 1.0
+    return L
+
 def rankMoves(moves):
     '''orders moves based on win percentage'''
     ranked = []
@@ -12,42 +45,9 @@ def rankMoves(moves):
         ranked = ranked[:i] + [move] + ranked[i:]
     return ranked
 
-def updateMemory():
-    '''updates ranking of moves in memory'''
-    for board in memory:
-        memory[board] = rankMoves(memory[board])
-
 def chooseMove(b):
     '''uses weighted probabilities to choose move'''
-    for board in memory:
-        if Board.is_isomorphic(board,b):
-            for move in memory[board]:
-                if random.random() < move[3]:
-                    return Board.matchMove(board,b,move[0])
-            return Board.randomMove(b)
-        else:
-            return Board.randomMove(b)
-
-def updateGame(game,outome):
-    '''updates memory with game moves and outcomes'''
-    if outcome == "w":
-        o = 1
-    else:
-        o = 2
-    for position in game:
-        newPosition = True
-        for board in memory:
-            if Board.is_isomorphic(position,board):
-                play = Board.matchMove(position,board,game[position])
-                newPlay = True
-                for move in memory[board]:
-                    if move[0] == play:
-                        move[o] += 1.0
-                        newPlay = False
-                if newPlay == True:
-                    memory[board] += [play,0.0,0.0,0.0]
-                    memory[board][-1][o] += 1.0
-                newPosition = False
-        if newPosition == True:
-            memory[position] = [[game[position],0.0,0.0,0.0]]
-            memory[board][0][o] += 1.0
+    for move in rankMoves(findMemory(b)):
+        if random.random() < move[3]:
+            return move[3]
+    return Board.randomMove(b)
