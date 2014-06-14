066541ba7417d441463b3e5e1323aeb3e7843aef
Jake Waksbaum <jake.waksbaum@gmail.com>
2014-06-14 14:00:31 -0400

    fix toggle

--- frontend/index.js
+++ frontend/index.js
@@ -1,9 +1,12 @@
-pieces = $(".piece")
-pieces.click(function(){
+function toggle(){
 	$(".piece").each(function(i){
 		$(this).toggleClass('selected')
+		$(this).off()
+		$(".piece").not('.selected').click(toggle)
 	})
-})
+}
+
+$(".piece").not('.selected').click(toggle)
 play = $("h2")
 play.click(function(){
 	piece = $(".piece.selected").html()
