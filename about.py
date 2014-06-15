4a1b8234e86ed8e79ef668c33ec043b4da19cebc
Ariel Levy <ariel.s.levy1@gmail.com>
2014-06-14 20:29:03 -0400

    editing about

--- about.py
+++ about.py
@@ -57,3 +57,63 @@ print '''
 '''
 
 # table row with daily statistics
+
+print '''
+</table>
+        
+        <br>
+        <h2>Search a Date</h2>
+        <table>
+            <tr>
+                <td>
+                    Year:
+                    <select name="year" size="1">
+'''
+
+# option menu with years, current selected
+
+print '''
+</select>
+                </td>
+                <td>
+                    Month:
+                    <select name="month" size="1">
+'''
+
+# option menu with months, current selected
+
+print '''
+</select>
+                </td>
+                <td>
+                    Day:
+                    <select name="day" size="1">
+'''
+
+# option menu with days, current selected
+
+print '''
+</select>
+                </td>
+                <td>
+                    <input type="submit" name="find" value="Find!">
+                </td>
+            </tr>
+        </table>
+        
+        <br>
+        <h2>Show All Data</h2>
+        <br>
+        <input type="submit" name="all" value="Show">
+'''
+
+# shows data table for every 100 games in memory
+
+print '''
+<br>
+        <h2>created by</h2> <h3>Jake Waksbaum</h3> <h2>and</h2> <h3>Ariel Levy</h3>
+        <br><br>
+        <a href="index.html">Return</a>
+    </body>
+</html>
+'''
