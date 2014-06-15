e1813b8db889172677bc58eb3ef99e2fec99a39b
Jake Waksbaum <jake.waksbaum@gmail.com>
2014-06-15 18:03:19 -0400

    changed index again

--- frontend/index.js
+++ frontend/index.js
@@ -1,15 +1,15 @@
 function toggle(){
-	$(".piece").each(function(i){
-		$(this).toggleClass('selected')
-		$(this).off()
-		$(".piece").not('.selected').click(toggle)
-	})
+    $(".piece").each(function(i){
+        $(this).toggleClass('selected')
+        $(this).off()
+    })
+    $(".piece").not('.selected').click(toggle)
 }
 
 $(".piece").not('.selected').click(toggle)
 play = $("h2")
 play.click(function(){
-	piece = $(".piece.selected").html()
-	$("form #piece").val(piece)
-	$("form").submit()
+    piece = $(".piece.selected").html()
+    $("form #piece").val(piece)
+    $("form").submit()
 })
