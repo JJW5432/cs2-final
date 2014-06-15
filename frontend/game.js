0e3dfcaff72948be5e0f89a12dda29ac7a725023
Jake Waksbaum <jake.waksbaum@gmail.com>
2014-06-15 17:20:59 -0400

    table resize

--- frontend/game.js
+++ frontend/game.js
@@ -11,6 +11,18 @@ if (piece === "O") {
 cells.click(makeMove)
 
 
+function tableResize() {
+                var w = $(window).width(),
+                    h = $(window).height(),
+                    table = $("table"),
+                    d = Math.min(w,h);
+                table.width(0.9*d);
+                table.height(0.9*d)
+            }
+            tableResize();
+            $(window).resize(tableResize)
+
+
 function state(cell) {
     if (cell.innerHTML === user) {return -1}
     else if (cell.innerHTML === computer) {return 1}
@@ -18,83 +30,83 @@ function state(cell) {
 }
 
 function getBoard() {
-    board = '';  
+    board = '';
     cells.each(function(){
-	board += String(state(this)) + ','
+    board += String(state(this)) + ','
     })
-	return board.slice(0,-1)
+    return board.slice(0,-1)
 }
 
 function makeMove(event) {
     cells.off()
     var cell = event.target
     if (state(cell) === 0) {
-	board = getBoard()
-	memory.push("\""+getBoard()+"\","+cell.id+",-1,");
-	cell.innerHTML = user;
-	$.ajax({
-	    type: 'POST',
-	    url: './play.py',
-	    data: {'board':getBoard()},
-	    success: function(data){
-		move = data.trim()
-		if ('123456789'.search(move) != -1) {
-		    console.log(move)
-	    	    move_cell = document.getElementById(move)
-		    memory.push("\""+getBoard()+"\","+move_cell.id+",1,")
-		    move_cell.innerHTML = computer;
-		}
-		else {endGame(move)}   
-		cells.click(makeMove)
-	    },
-	    async:true
-	});
-	//console.log(move)
+    board = getBoard()
+    memory.push("\""+getBoard()+"\","+cell.id+",-1,");
+    cell.innerHTML = user;
+    $.ajax({
+        type: 'POST',
+        url: './play.py',
+        data: {'board':getBoard()},
+        success: function(data){
+        move = data.trim()
+        if ('123456789'.search(move) != -1) {
+            console.log(move)
+                move_cell = document.getElementById(move)
+            memory.push("\""+getBoard()+"\","+move_cell.id+",1,")
+            move_cell.innerHTML = computer;
+        }
+        else {endGame(move)}
+        cells.click(makeMove)
+        },
+        async:true
+    });
+    //console.log(move)
     }
 }
 
     function endGame(winner) {
-	console.log(winner)
-	var el = $("#overlay"),
-	p = el.find('p'),
-	winner = winner.split('\n'),
-	outcome;
-	el.toggleClass('visible')
-	lane = winner[1].split(',')
-	if (winner[0] === "user") {
-	    outcome = -1
-	    p.html("You&rsquo;ve won!");
-	    p.addClass('win');
-	    $.each(lane,function(i, val){
-		$("#"+val).addClass('win')
-	    })
-		} else if (winner[0] === "computer") {
-		    outcome = 1
-		    p.html("Sorry you lost!");
-		    p.addClass('lose');
-		    $.each(lane,function(i, val){
-			cell = $("#"+val)
-			cell.html(computer)
-			cell.addClass('lose')
-		    })
-			} else if (winner[0] === "tie") {
-			    outcome = 0
-			    p.html("A tie!");
-			    p.addClass('tie');
-			    $.each(lane,function(i, val){
-				cell = $("#"+val)
-				cell.html(computer)
-			    })
-			}
-	cells.off('click')
-	$.each(memory, function(i, val){
-	    memory[i] = val + String(outcome) + ',';
-	})
-	    $.ajax({
-		type: 'POST',
-		url: './memory.py',
-	    data: {'memory':memory.join('\n')},
-		success: function(data){console.log(data)},
-		async: true
-	    });
+    console.log(winner)
+    var el = $("#overlay"),
+    p = el.find('p'),
+    winner = winner.split('\n'),
+    outcome;
+    el.toggleClass('visible')
+    lane = winner[1].split(',')
+    if (winner[0] === "user") {
+        outcome = -1
+        p.html("You&rsquo;ve won!");
+        p.addClass('win');
+        $.each(lane,function(i, val){
+        $("#"+val).addClass('win')
+        })
+        } else if (winner[0] === "computer") {
+            outcome = 1
+            p.html("Sorry you lost!");
+            p.addClass('lose');
+            $.each(lane,function(i, val){
+            cell = $("#"+val)
+            cell.html(computer)
+            cell.addClass('lose')
+            })
+            } else if (winner[0] === "tie") {
+                outcome = 0
+                p.html("A tie!");
+                p.addClass('tie');
+                $.each(lane,function(i, val){
+                cell = $("#"+val)
+                cell.html(computer)
+                })
+            }
+    cells.off('click')
+    $.each(memory, function(i, val){
+        memory[i] = val + String(outcome) + ',';
+    })
+        $.ajax({
+        type: 'POST',
+        url: './memory.py',
+        data: {'memory':memory.join('\n')},
+        success: function(data){console.log(data)},
+        async: true
+        });
     }
